7009f4cb0df04332bdfe10580b7e94feda612ae7
syuta <nakamura.shuta@gmail.com>
2011-01-28 14:17:40 +0900

    change

--- client/socket.js
+++ client/socket.js
@@ -6,7 +6,7 @@
 
 //var socket = new io.Socket("192.168.1.39",{port:8080}); 
 //var socket = new io.Socket("localhost",{port:8080}); 
-var socket = new io.Socket("mytaskboardsample.nodester.com",{port:8242}); 
+var socket = new io.Socket("taskboardsample.nodester.com",{port:824}); 
 
 socket.connect();
 
